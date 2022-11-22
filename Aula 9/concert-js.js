1fa4b1f295ac3c509fb44eca384b71171c326cb0
Mathias Ranna <mathiasranna@gmail.com>
2022-11-22 13:36:55 +0000

    Aula 9

--- Aula 9/concert-js.js
+++ Aula 9/concert-js.js
@@ -1,9 +1,47 @@
-var forms = document.getElementById("#main-form");
+const form = document.getElementById("mainForm");
+const checkboxes = document.getElementsByName("ticket-class");
+let checkboxCount = 0;
+let ticketsValue = 0;
 
-console.log(forms)
 
-// $(document).ready(function (){
-//    $('.needs-validation').addEventListener('submit', function (event){
-//
-//    })
-// })
+$(document).ready(function () {
+    form.addEventListener('submit', function (event) {
+
+        // Count how many checkboxes have been checked
+        checkboxes.forEach(function (checkbox) {
+            checkboxCount += checkbox.checked ? 1 : 0;
+        })
+
+        // Validate checkboxes depending on how many are checked
+        if (checkboxCount < 2) {
+            checkboxes.forEach(function (checkbox) {
+                checkbox.required = true;
+            })
+        } else {
+            checkboxes.forEach(function (checkbox) {
+                checkbox.required = false;
+            })
+        }
+
+        // check main validation of forms
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+    })
+})
+
+$('input[type=checkbox]').change(function (event) {
+    if (event.target.checked) {
+        ticketsValue += parseInt(event.target.value);
+    } else {
+        ticketsValue -= parseInt(event.target.value);
+    }
+
+    if (ticketsValue === 0) {
+        $('#totalValue').val("");
+    }
+    $('#totalValue').val(ticketsValue.toString() + "€");
+})
+
